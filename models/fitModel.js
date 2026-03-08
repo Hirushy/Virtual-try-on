7021209922630e3c8be23b252b08e50b8f46eb86
Hirushi Perera <hiruship760@gmail.com>
2026-03-08 13:12:47 +0530

    model

---
+++ models/fitModel.js
@@ -0,0 +1,21 @@
+export const FIT_ZONE_MODEL = {
+  shoulders: {
+    bodyKey: "Shoulder",
+    idealRange: 0.8
+  },
+
+  chest: {
+    bodyKey: "Chest",
+    idealRange: 0.8
+  },
+
+  torso: {
+    bodyKey: "Torso",
+    idealRange: 0.9
+  },
+
+  collar: {
+    bodyKey: "Neck",
+    idealRange: 0.7
+  }
+};
